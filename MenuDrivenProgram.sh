f1336b206e2f809082e34dd7a2484dbf740dac8a
Himavarshini Yarragangu <himayarragangu@gmail.com>
2018-03-11 11:48:26 +0530

    A menu driven Program to edit, compile or run
    All the assignment programs chosen from a menu driven program can be edited, compiled and run.

---
+++ MenuDrivenProgram.sh
@@ -0,0 +1,510 @@
+#!/bin/bash
+#Group members: Y.Himavarshini,R.Anitha Sai Saranya, M.Usha
+pause(){
+  read -p "Press [Enter] key to continue..." fackEnterKey
+}
+
+one(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 1.sh ;;
+    2) chmod +x 1.sh
+       ./1.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+two(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 2.sh ;;
+    2) chmod +x 2.sh
+       ./2.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+# do something in three()
+three(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 3.sh ;;
+    2) chmod +x 3.sh
+       ./3.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+#do something in folder of four()
+four(){
+  cd #path of the file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 4.sh ;;
+    2) chmod +x 4.sh
+       ./4.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+#do something in five()
+five(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 5.sh ;;
+    2) chmod +x 5.sh
+       ./5.sh 1 4 ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+
+six(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 6.sh ;;
+    2) chmod +x 6.sh
+       ./6.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+seven(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 1.sh ;;
+    2) chmod +x 1.sh
+       ./1.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+eight(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 2.sh ;;
+    2) chmod +x 2.sh
+       ./2.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+nine(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 3.sh ;;
+    2) chmod +x 3.sh
+       ./3.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+ten(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 4.sh ;;
+    2) chmod +x 4.sh
+       ./4.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+eleven(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 5.sh ;;
+    2) chmod +x 5.sh
+       ./5.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+factorial(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 6.sh ;;
+    2) chmod +x 6.sh
+       ./6.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+oddbetweentwonumbers(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 7.sh ;;
+    2) chmod +x 7.sh
+       ./7.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+squarerectangle(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 8.sh ;;
+    2) chmod +x 8.sh
+       ./8.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+lowesthighest(){
+  cd #path of the folder of file
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 9.sh ;;
+    2) chmod +x 9.sh
+       ./9.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+multtable(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 10.sh ;;
+    2) chmod +x 10.sh
+       ./10.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+menudrivencalc(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit or execute [1-2]"
+  read x
+  case $x in
+    1) gedit 11.sh ;;
+    2) chmod +x 11.sh
+       ./11.sh ;;
+    3) exit 0 ;;
+  esac
+  pause
+}
+
+
+ph2(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 1.c ;;
+    2) gcc 1.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+ph3(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 2.c ;;
+    2) gcc 2.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+ph4(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 3.c ;;
+    2) gcc 3.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+shmm1(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 1.c ;;
+    2) gcc 1.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+shmm2(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 2.c ;;
+    2) gcc 2.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+shmm3(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 3.c ;;
+    2) gcc 3.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+shmm4(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 4.c ;;
+    2) gcc 4.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+shmm5(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 51.c ;;
+    2) gcc 51.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+pipeshm1(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 27.c ;;
+    2) gcc 27.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+pipeshm2(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit 28.c ;;
+    2) gcc 28.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+fcfs(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit fcfs.c ;;
+    2) gcc fcfs.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+fcfsdifat(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit FCFSDifAT.c ;;
+    2) gcc FCFSDifAT.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+prioritypreemptive(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit prioritypreemptive.c ;;
+    2) gcc prioritypreemptive.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+prioritynonpreemptive(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit prioritynonpreemptive.c ;;
+    2) gcc prioritynonpreemptive.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+roundrobin(){
+  cd #path of the folder of file 
+  echo "Enter your choice to edit, compile, execute [1-3]"
+  read x
+  case $x in
+    1) gedit roundrobin.c ;;
+    2) gcc roundrobin.c ;;
+    3) ./a.out ;;
+    4) exit 0 ;;
+  esac
+  pause
+}
+# function to display menus
+show_menus() {
+	clear
+	echo "~~~~~~~~~~~~~~~~~~~~~"
+	echo " M A I N - M E N U"
+	echo "~~~~~~~~~~~~~~~~~~~~~"
+	echo "1. Write a shell script to ask your name, program name and enrollment number and print it on the screen"
+	echo "2. Write a shell script to find the sum, the average and the product of the four integers entered."
+	echo "3. Write a shell program to exchange the values of two variables."
+	echo "4. Write a shell program to concatenate two strings and find the length of the resultant string"
+	echo "5. Write a shell program to add, subtract and multiply the 2 given numbers passed as command line arguments"
+  echo "6. Write a shell program to concatenate the contents of 2 files"
+  echo "7. Find the lines containing a number in a file."
+  echo "8. Write a shell program to count the characters, count the lines and the words in a particular file"
+  echo "9. Write a shell script that would wait 5 seconds and then display the time"
+  echo "10. Write a shell program to read a number (*such as 123) and find the sum of digits (1+2+3=6)."
+  echo "11. Write a shell program to read a number and display reverse the number. For example, 123 should be printed as 321."
+  echo "12. Write a shell script to find the factorial of a number"
+  echo "13. Write a shell program to read two numbers and display all the odd numbers between those two numbers."
+  echo "14. Write a shell script that will calculate the area and perimeter of the rectangle and the area and circumference of the circle"
+  echo "15. Given a file of numbers, write a shell script that will find the lowest and highest number. "
+  echo "16. Write a shell script for printing multiplication table for a given number"
+  echo "17. Write down a shell script to make a menu driven calculator using case."
+  echo "19. Create the following processes hierarchy using fork and in each process print process id, parent process id.
+          Parent
+              \
+                Child1
+                  \
+                    Child2"
+  echo "20. Create the following processes hierarchy using fork and in each process print process id, parent process id.
+                Parent
+                /   \
+            Child1  Child2
+                    /   \
+                Child3   Child4"
+  echo "21. Create the following processes hierarchy using fork and in each process print process id, parent process id.
+                     Process1
+                  /     |     \
+          Process2 	 Process3  Process4
+                               /      \
+                          Process5	Process6"
+  echo "22. Write a C program that allows communication between a parent process and child process using pipe.Parent process has to take input n from the user, where n is an integer. The parent process should then write n to the pipe. The child process has to read the contents of the pipe and then print all the odd numbers till the limit n."
+  echo "23. Write a C program that allows communication between a parent process and child process using pipe.Parent process has to take input string from the user.The child process has to read the contents of the pipe and convert it to capital letters"
+  echo "24. Write a C program that allows communication between a parent process and child process using Shared Memory.
+a. Parent process has to take input n from the user, where n is an integer.
+b. The parent process should then write n to the shared memory.
+c. The child process has to read the contents of the shared memory and then print all the odd
+numbers till the limit n"
+  echo "25.  Write a C program that allows communication between a parent process and child process
+using shared memory
+a. Parent process has to take input string from the user.
+b. The child process has to read the contents of the shared memory and convert it to capital
+letters and print it. "
+  echo "26. Write a program that creates a shared memory segment and waits until two other separate processes writes something into that shared memory segment after which it prints what is written in shared memory. For the communication between the processes to take place assume that the process 1 writes 1 in first position of shared memory and waits; process 2 writes 2 in first position of shared memory and goes on to write 'hello' and then process 3 writes 3 in first position of shared memory and goes on to write 'memory' and finally the process 1 prints what is in shared memory written by two other processes."
+  echo "27. 1. Write a program to implement the following process hierarchy:
+                                         Parent
+                                         /     \
+	                                  Child1     Child2
+  Parent: Input a number
+  Child1: Check whether the number is Armstrong or not and return that result to parent.
+  Child2: Check whether the number is perfect or not"
+  echo "28. Implement IPC using shared memory. Accept 5 numbers in program1, and in program 2 display the same in ascending order and also find the sum of these numbers."
+  echo "29. FCFS with same arrival times"
+  echo "30. FCFS with different arrival times"
+  echo "31. Priority Scheduling- Pre-emptive"
+  echo "32. Priority Scheduling- Non Pre-emptive"
+  echo "33. Implement Round Robin Scheduling Algorithm for a given time-slice quantum"
+	echo "34. Exit"
+}
+read_options(){
+	local choice
+	read -p "Enter choice [ 1 - 33] and 34 to Exit" choice
+  case $choice in
+		1) one ;;
+		2) two ;;
+		3) three ;;
+		4) four ;;
+		5) five ;;
+		6) six ;;
+    7) seven ;;
+    8) eight ;;
+    9) nine ;;
+    10) ten ;;
+    11) eleven ;;
+    12) factorial ;;
+    13) oddbetweentwonumbers ;;
+    14) squarerectangle ;;
+    15) lowesthighest ;;
+    16) multtable ;;
+    17) menudrivencalc ;;
+    19) ph2 ;;
+    20) ph3 ;;
+    21) ph4 ;;
+    22) shmm1 ;;
+    23) shmm2 ;;
+    24) shmm3 ;;
+    25) shmm4 ;;
+    26) shmm5 ;;
+    27) pipeshm1 ;;
+    28) pipeshm2 ;;
+    29) fcfs ;;
+    30) fcfsdifat ;;
+    31) prioritypreemptive ;;
+    32) prioritynonpreemptive ;;
+    33) roundrobin ;;
+    34) exit 0;;
+		*) echo -e "Error..." && sleep 2
+	esac
+}
+while true
+do
+	show_menus
+	read_options
+done
